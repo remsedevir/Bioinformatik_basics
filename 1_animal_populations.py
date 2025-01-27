1d00e3483e44016e98a46c47ea332569ca70a382
Caroline <121579572+remsedevir@users.noreply.github.com>
2025-01-27 17:57:40 +0100

    Update 1_animal_populations.py
    organising the comments

--- 1_animal_populations.py
+++ 1_animal_populations.py
@@ -6,20 +6,6 @@ species_data = [
     {'name': 'Panda', 'populations': {'China': 1800}},
 ]
 
-# def total_pop_calculator(species_data):
-#     for species in species_data:
-#         total_pop = sum(species['populations'].values())  # Sum all the population values
-#         print(f"a) Total population of {species['name']}: {total_pop}")
-
-# def highest_population(species_data):
-#     highest_pop = 0
-#     for species in species_data:
-#         total_pop = sum(species['populations'].values()) # population of first species
-#         if total_pop > highest_pop : 
-#             highest_pop = total_pop # remember the highest population
-#             animal = species
-#     print(f"b) {species['name']} has the highest population with a total of {highest_pop} individuals.") 
-
 print("a)")
 for species in species_data:
     total_pop = sum(species['populations'].values())  # Sum all the population values
@@ -34,4 +20,27 @@ for species in species_data:
         animal = species
  
 print(f"b) {animal['name']}s have the highest population with a total of {highest_pop} individuals.") 
+
+
+
+
+
+
+
+
+
+# def total_pop_calculator(species_data):
+#     for species in species_data:
+#         total_pop = sum(species['populations'].values())  # Sum all the population values
+#         print(f"a) Total population of {species['name']}: {total_pop}")
+
+# def highest_population(species_data):
+#     highest_pop = 0
+#     for species in species_data:
+#         total_pop = sum(species['populations'].values()) # population of first species
+#         if total_pop > highest_pop : 
+#             highest_pop = total_pop # remember the highest population
+#             animal = species
+#     print(f"b) {species['name']} has the highest population with a total of {highest_pop} individuals.") 
+
     
