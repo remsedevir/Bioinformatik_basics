0e0d439c76fb7d6772516ecf851c3777435e96f7
Caroline <121579572+remsedevir@users.noreply.github.com>
2025-02-03 13:04:19 +0100

    Create BLAST.py

---
+++ BLAST.py
@@ -0,0 +1,35 @@
+import random
+
+base = ["A", "T", "G", "C"]
+#primer = "TAT"
+
+# nuc = random.randint(0,3) # wichtig!!
+
+def generate_sequence(k):
+    sequence = ""
+    for i in range (0,k):
+        nuc = random.randint(0,3)
+        sequence = sequence + base[nuc]
+    return sequence
+
+
+#dna = generate_sequence() # spart Schreibaufwand, sorgt dafür, dass wir hier nicht aus Versehen eine neue Sequenz bilden 
+primer = generate_sequence(2)
+print(primer)
+dna = generate_sequence(200)
+print(dna)
+
+
+for i in range(0,len(dna)-3):   
+    n = 0
+    p = 0
+    if dna[i] == primer[0]:
+        i = p
+        n += 1
+        while dna[p] == primer[n]: # idk why that's not working
+            n += 1
+            p += 1
+            if n == len(primer):
+                break
+        if dna[p] == primer[-1]:
+            print(i)
