d958706592642f56ca47e2a45fb635aab0ca57ad
Caroline <121579572+remsedevir@users.noreply.github.com>
2025-02-03 18:12:52 +0100

    4_palindromic_sequences.py erstellen

---
+++ 4_palindromic_sequences.py
@@ -0,0 +1,16 @@
+# "alledged weird bioinformatics question made by chatgpt"
+
+sequence = "" # insert palindrome here
+
+i=1
+while i <= len(sequence)
+    if sequence[i-1] == sequence[-i]:
+        i += 1
+        
+    else:
+        i = len(sequence)
+        print("The given sequence is not palindromic.")
+    
+    
+# i forgot to check if it's a reverse palindrome, but i'm getting carsick so i'll do it later
+
