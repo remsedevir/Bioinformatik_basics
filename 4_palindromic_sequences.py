d398bb4f1386db9a6e5be7223b954a082f2f2071
Caroline <121579572+remsedevir@users.noreply.github.com>
2025-02-04 11:03:54 +0100

    Update 4_palindromic_sequences.py
    fixed, also cool solution

--- 4_palindromic_sequences.py
+++ 4_palindromic_sequences.py
@@ -1,16 +1,21 @@
 # "alledged weird bioinformatics question made by chatgpt"
 
-sequence = "" # insert palindrome here
+sequence = "" # insert dna here
 
-i=1
-while i <= len(sequence)
-    if sequence[i-1] == sequence[-i]:
-        i += 1
+base = {"A":"T", "T":"A", "G":"C", "C":"G"}
+
+def reverse_sequence(sequence):
+    r_sequence = ""
+    for i in sequence:
+        r_sequence += base[i]
+    return r_sequence
+
+reversed = reverse_sequence(sequence)
+
+if sequence == reversed[::-1]: # remember this, very cool
+    print("The given sequence is palindromic.")
         
-    else:
-        i = len(sequence)
-        print("The given sequence is not palindromic.")
+else:
+    print("The given sequence is not palindromic.")
     
-    
-# i forgot to check if it's a reverse palindrome, but i'm getting carsick so i'll do it later
 
